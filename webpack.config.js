1e0cc41b772673155940c53b2bd030dc652f64ff
agustinbalquin <agustinbalquin@gmail.com>
2016-08-29 23:54:56 -0700

    updated config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+//////////
+//Config//
+//////////
+
+
+//required for webpack 
+const webpack = require('webpack');
+const fs      = require('fs');
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve;
+
+const getConfig = require('hjs-webpack');
